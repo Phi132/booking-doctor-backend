48fe31a190eea58f818ab119ceadeda60640b010
tphi10761 <tphi10761@gmail.com>
2021-12-29 10:06:24 +0700

    connect DB without config.json... use env file

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,21 +1,25 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('dktmrfbrues8i', 'zbyzxelmjhtjqy',
-    '2031eaee356898ce6bacc81608142da00d5c8cf6d9742a91b8c4222def6e745c', {
-    host: 'ec2-54-147-76-191.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    logging: false,
-    query: {
-        raw: true
-    },
-    timezone: '+07:00',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+        query: {
+            raw: true
+        },
+        timezone: '+07:00',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
-    }
-});
+    });
 
 const connectDB = async () => {
     try {
--- src/models/index.js
+++ src/models/index.js
@@ -6,16 +6,40 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
+// không cần dùng file config.json nữa
 const db = {};
 
+let customConfig = {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: false,
+  query: {
+    raw: true
+  },
+  timezone: '+07:00',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+
+  }
+}
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  customConfig);
+
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
